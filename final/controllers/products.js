88aba5b9dfb54bd93758c45285162b22863d7bf4
smajaas <smajaas@gmail.com>
2023-09-30 16:24:26 +0530

    select function

--- final/controllers/products.js
+++ final/controllers/products.js
@@ -1,11 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).select('name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
   if (featured) {
@@ -19,11 +19,17 @@ const getAllProducts = async (req, res) => {
   }
   // console.log(queryObject);
   let result = Product.find(queryObject);
+  //Sort
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
   }
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
